5e435e09740b367c6c7f3300cdf8a696314c9e48
Bro-nya <1572205083@qq.com>
2025-06-07 00:51:12 +0800

    提交12

---
+++ src/STU05_List列表/MainTset.java
@@ -0,0 +1,11 @@
+package STU05_List列表;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainTset {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("AAAA");
+    }
+}
